f9ef875bb4cb008d44041cc9ee00c1adff0493b7
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-08 17:17:06 -0300

    feat: add route to vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
+
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
+app.register(cookie, { secret: "expert-votes", hook: "onRequest" });
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => console.log("HTTP server is running 🔥"));
